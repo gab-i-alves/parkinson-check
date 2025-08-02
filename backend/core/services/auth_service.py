35c263969a362586befbc2d723bc7264d2597b92
Luan Pinheiro <luluka2909@gmail.com>
2025-08-02 13:18:07 -0300

    chore: simplifying auth email field

--- backend/core/services/auth_service.py
+++ backend/core/services/auth_service.py
@@ -7,8 +7,8 @@ from core.services.user_service import get_user_by_email
 from ..security.token import create_access_token
 
 def login(login_form: LoginFormRequest):
-    user = get_user_by_email(login_form.email_or_cpf)
+    user = get_user_by_email(login_form.email)
     if(not user or not verify_password(login_form.password, user.hashed_password)):
         raise HTTPException(HTTPStatus.UNAUTHORIZED, detail="Verifique as credenciais de acesso.")
     
-    return create_access_token(data={'sub': login_form.email_or_cpf})
+    return create_access_token(data={'sub': login_form.email})
