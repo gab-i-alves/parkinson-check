e331fe8e9a0ec2c954542773b6021dcb507ebd15
GiovanniLMTenereli <giovannilmtenereli@gmail.com>
2025-08-24 15:26:11 -0300

    feat: preditor de parkison como pratica

--- backend/core/services/spiral_test_service.py
+++ backend/core/services/spiral_test_service.py
@@ -1,5 +1,15 @@
 from api.schemas.tests import SpiralImageSchema, SpiralPracticeTestResult
-
-
-def process_spiral_as_practice(image: SpiralImageSchema) -> SpiralPracticeTestResult:
-    ...
+from ..parkinson_classifier.image_classifier.predictor import process_spiral_image
+def process_spiral_as_practice(schema: SpiralImageSchema) -> SpiralPracticeTestResult:
+    result = process_spiral_image(schema.image)
+    
+    if result >= 0.99:
+        analysis = "Parkinson"
+    elif result >= 0.6:
+        analysis = "Chance de Parkinson"
+    elif result >= 0.4:
+        analysis = "Baixa chance de Parkison"
+    else:
+        analysis = "Normal"
+    
+    return SpiralPracticeTestResult(score=result, analysis=analysis)
