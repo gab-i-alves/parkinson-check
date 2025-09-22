db5becdd4d6dc1eaff8f7d116b13890b2093911c
Gabi Alves <gab.alvesnacimento@gmail.com>
2025-09-22 02:38:52 -0300

    refactor: change test schema/model

--- backend/api/schemas/tests.py
+++ backend/api/schemas/tests.py
@@ -1,24 +1,34 @@
 from fastapi import File, HTTPException, UploadFile
 from pydantic import BaseModel, model_validator
+from typing import Dict, Optional
 
 class SpiralImageSchema(BaseModel):
-    image: UploadFile
+    image: bytes
 
-    class Config:
-        arbitrary_types_allowed = True
+    # class Config:
+    #     arbitrary_types_allowed = True
         
-    @model_validator(mode="after")
-    def check_if_is_image(self):
-        if not self.image.content_type.startswith("image/"):
-            raise HTTPException(status_code=400, detail="Arquivo deve ser uma imagem")
+    # @model_validator(mode="after")
+    # def check_if_is_image(self):
+    #     if not self.image.content_type.startswith("image/"):
+    #         raise HTTPException(status_code=400, detail="Arquivo deve ser uma imagem")
 
     @classmethod
     def as_form(
         cls,
         image: UploadFile = File(...)
     ):
-        return cls(image=image)
+        return cls(image=image.file.read())
         
+class ModelPrediction(BaseModel):
+    prediction: str
+    probabilities: Optional[Dict[str, float]] = None
+
+class SpiralTestVoteCount(BaseModel):
+    Healthy: int
+    Parkinson: int
+
 class SpiralPracticeTestResult(BaseModel):
-    score: float
-    analysis: str
+    majority_decision: str
+    vote_count: SpiralTestVoteCount
+    model_results: Dict[str, ModelPrediction]
