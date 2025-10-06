a7820fa6fd5300aea765725232a1c822fa97f671
Gabi Alves <gab.alvesnacimento@gmail.com>
2025-10-06 16:42:00 -0300

    fix: trying to validate cookies in prod

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.router import api_router
-from infra.db.connection import get_session
+from infra.settings import settings 
 
 app = FastAPI(title="ParkinsonCheck API")
 
-origins = [
-    "https://parkinson.gabi-alves.com",  # frontend on railway
-    "http://localhost:4200"              # local development
-]
+origins = []
+if settings.ENVIRONMENT == "production":
+    origins.append("https://gabi-alves.com")
+    origins.append("https://www.gabi-alves.com")
+else:
+    origins.append("http://localhost:4200")
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex="https://.*\.gabi-alves\.com",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
