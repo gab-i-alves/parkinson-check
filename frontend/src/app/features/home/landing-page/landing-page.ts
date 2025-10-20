5011da2bd8f672e951913852ab7c20fc02edba39
gabi <gab.alvesnacimento@gmail.com>
2025-10-20 01:53:45 -0300

    update landing page design (more minimal)

--- frontend/src/app/features/home/landing-page/landing-page.ts
+++ frontend/src/app/features/home/landing-page/landing-page.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './landing-page.html',
 })
 export class LandingPage {}
