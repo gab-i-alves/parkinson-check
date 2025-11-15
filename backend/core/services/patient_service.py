c7d4ac40f78a8d52b2f4c28b28a0aaeee4394089
gabi <97996243+gab-i-alves@users.noreply.github.com>
2025-11-15 15:00:33 -0300

    feat: add message support to binding requests and patient gender field

--- backend/core/services/patient_service.py
+++ backend/core/services/patient_service.py
@@ -174,6 +174,7 @@ def get_binded_patients(session: Session, current_user: User) -> list[PatientLis
                 bind_id=bind_id,
                 age=age,
                 status=status,
+                gender=patient.gender,
             )
         )
     return patient_list
@@ -402,6 +403,7 @@ def get_patients(session: Session, doctor: User, parameters: GetPatientsSchema) 
                 bind_id=None,
                 age=age,
                 status=status,
+                gender=patient.gender,
             )
         )
 
